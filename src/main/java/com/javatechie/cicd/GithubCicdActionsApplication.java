e494316da47956f7e5ed7217ceea12f063c51da3
jhonny1997str <167597745+jhonny1997str@users.noreply.github.com>
2024-11-07 17:33:07 -0500

    Jhonny

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to God's salvation. ";
+		return "Welcome to God's salvation.. ";
 	}
 
 //	echo "# github-actions-example" >> README.md
